cb810065ec501aed8cb5ab4bf7d28c6363dfba93
MeghanaBMadlur <144421654+MeghanaBMadlur@users.noreply.github.com>
2025-05-13 11:43:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,27 @@
 <?php
-$name = $_POST['name'];
-$sem = $_POST['sem'];
-$mail=$_POST['mail'];
-$gender=$_POST['Gender'];
-$tel=$_POST['tel'];
-$agree = isset($_POST['agree']) ? "Yes" : "No";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"] ?? "";
+    $sem = $_POST["sem"] ?? "";
+    $mail = $_POST["mail"] ?? "";
+    $gender = $_POST["Gender"] ?? "";
+    $tel = $_POST["tel"] ?? "";
 
-
-
-$docContent = "Name: $name\nGender: $gender\n Sem:$sem \n Mail:$mail \n Tel:$tel \n \n ";
-file_put_contents("user_data.doc", $docContent, FILE_APPEND);
-
-echo "Data saved successfully!";
+    // Save data (you can add code here if saving to file/database)
+    echo "Data saved successfully!";
+} else {
+    // Show the form when the page is opened
+    ?>
+    <h2>Student Form</h2>
+    <form method="POST">
+        Name: <input type="text" name="name"><br><br>
+        Semester: <input type="text" name="sem"><br><br>
+        Email: <input type="email" name="mail"><br><br>
+        Gender:
+        <input type="radio" name="Gender" value="Male"> Male
+        <input type="radio" name="Gender" value="Female"> Female<br><br>
+        Phone: <input type="text" name="tel"><br><br>
+        <input type="submit" value="Submit">
+    </form>
+    <?php
+}
 ?>
